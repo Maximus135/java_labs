19a9dec5c668c8401ae39554c75b8dd9f4fed427
Maximus135 <maxim.bashilov@yandex.ru>
2020-03-02 15:46:18 +0300

    lab_2 task 1

--- lab2/GenericItem.java
+++ lab2/GenericItem.java
@@ -4,8 +4,12 @@ public class GenericItem {
     public int ID;
     public String name;
     public float price;
+    public int group; // для храниние информации об аналогах товара (группировка) 
+    public enum Category {FOOD, PRINT, DRESS, GENERAL};
+    
+    public Category category = Category.GENERAL;
 
     public void printAll() {
-        System.out.printf("ID: %d , Name: %-20s , price:%5.2f \n", ID, name, price);
+        System.out.printf("ID: "+ID+" , Name: "+name+" , price:"+price+" , category: "+category+" \n", ID, name, price);
     }
 }
--- lab2/Main.java
+++ lab2/Main.java
@@ -3,5 +3,23 @@ package lab2;
 public class Main {
     public static void main(final String[] args) {
 
+        GenericItem item_1 = new GenericItem();
+        GenericItem item_2 = new GenericItem();
+        GenericItem item_3 = new GenericItem(); 
+        
+        item_1.ID = 123;
+        item_1.name = "banana";
+        item_1.price = 12f;
+
+        item_2.ID = 231;
+        item_2.name = "bread";
+        item_2.price = 33f;
+
+        item_3.ID = 321;
+        item_3.name = "apple";
+        item_3.price = 10f;
+
+        item_1.printAll();
+
     }
 }
